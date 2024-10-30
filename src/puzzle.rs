267132640c5dfa58653a410b03e9cac84d629aab
Dan Plubell <Dan.Plubell@coxautoinc.com>
2024-10-30 16:49:46 -0500

    Updates

--- src/puzzle.rs
+++ src/puzzle.rs
@@ -1,15 +1,36 @@
-#[derive(Clone,PartialEq,Debug)]
-pub struct Puzzle([u8;81]);
+#[derive(Clone, PartialEq, Debug)]
+pub struct Puzzle(Vec<u8>);
 
-#[derive(Clone,PartialEq, Debug)]
-
-struct ParsePuzzleError(String);
+impl Puzzle {
+    fn new(values: &str) -> Self {
+        Puzzle::try_from(values).unwrap()
+    }
+}
+#[derive(Clone, PartialEq, Debug, thiserror::Error)]
+enum ParsePuzzleError {
+    #[error("Value string is too long")]
+    TooLong,
+    #[error("Value string is too short")]
+    TooShort,
+    #[error("Value string contains alphabetic characters")]
+    HasAlpha,
+}
 impl TryFrom<&str> for Puzzle {
     type Error = ParsePuzzleError;
-    fn try_from(value: &str) -> Result<Self,Self::Error> {
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
         if value.len() > 81 {
-            return Err(ParsePuzzleError("Value string is too long".to_string()))
+            return Err(ParsePuzzleError::TooLong);
         }
-        Ok(Puzzle([0;81]))
+        if value.len() < 81 {
+            return Err(ParsePuzzleError::TooShort);
+        }
+        if value.chars().any(|c| !c.is_numeric()) {
+            return Err(ParsePuzzleError::HasAlpha);
+        }
+        let n = value
+            .chars()
+            .map(|c| c.to_digit(10).unwrap() as u8)
+            .collect();
+        Ok(Puzzle(n))
     }
 }
