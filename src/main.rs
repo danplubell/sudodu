be8464dd741b28649d9ef3011de49384826d7edd
Dan Plubell <danplubell@gmail.com>
2024-10-30 19:10:51 -0500

    Update

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,16 @@ mod sudoku;
 mod puzzle;
 mod solution;
 
-use csv::Reader;
-use std::error::Error;
+//use csv::Reader;
+//use std::error::Error;
 //use sudoku::Sudoku;
 
 fn main() {
-    let solution = "318457962572986143946312578639178425157294836284563791425731689761829354893645217";
-    let puzzle = "310450900072986143906010508639178020150090806004003700005731009701829350000645010";
+    let _solution = "318457962572986143946312578639178425157294836284563791425731689761829354893645217";
+    let _puzzle = "310450900072986143906010508639178020150090806004003700005731009701829350000645010";
     //let s = Sudoku::new(solution, puzzle);
-    let c: char = '8';
-    let b: u8 = c as u8;
-    let p = "1234";
-    let mut a = [0u8;4];
-    for (i,c) in p.chars().enumerate() {
-        a[i] = c as u8;
-    }
-    println!("{:?}", a);
 }
-
+/*
 fn example() -> Result<(), Box<dyn Error>> {
     let mut rdr = Reader::from_path("../sudoku.csv_save")?;
     let mut i = 0;
@@ -36,3 +28,6 @@ fn example() -> Result<(), Box<dyn Error>> {
 }
 
 
+
+
+ */
--- src/puzzle.rs
+++ src/puzzle.rs
@@ -1,11 +1,6 @@
 #[derive(Clone, PartialEq, Debug)]
 pub struct Puzzle(Vec<u8>);
 
-impl Puzzle {
-    fn new(values: &str) -> Self {
-        Puzzle::try_from(values).unwrap()
-    }
-}
 #[derive(Clone, PartialEq, Debug, thiserror::Error)]
 pub enum ParsePuzzleError {
     #[error("Value string is too long")]
@@ -19,7 +14,7 @@ impl TryFrom<&str> for Puzzle {
     type Error = ParsePuzzleError;
     fn try_from(value: &str) -> Result<Self, Self::Error> {
         if value.len() > 81 {
-            return Err(ParsePuzzleError::TooLong);
+            return Err(ParsePuzzleError::TooLong)
         }
         if value.len() < 81 {
             return Err(ParsePuzzleError::TooShort);
@@ -37,19 +32,22 @@ impl TryFrom<&str> for Puzzle {
 
 #[cfg(test)]
 mod tests {
-    use crate::puzzle::Puzzle;
+    use crate::puzzle::{ParsePuzzleError, Puzzle};
     use std::convert::TryFrom;
 
     #[test]
-    fn too_long(){
-        let status = Puzzle::try_from("1234").unwrap();
-        assert_eq!(status.0, vec!(1,2,3,4));
+    fn too_short(){
+        let err = Puzzle::try_from("1234").unwrap_err();
+        assert_eq!(err, ParsePuzzleError::TooShort);
 
     }
+    /*
     #[test]
     fn not_all_digits() {
-        let puzzle = Puzzle::try_from("abcd").unwrap();
+        let puzzle = Puzzle::try_from("12abc").unwrap();
         println!("{:?}",puzzle.0);
         assert_eq!(puzzle.0, vec!());
     }
+    
+     */
 }
