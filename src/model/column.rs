4db70b74838a44a3053e02d50e517a31ca2da030
Dan Plubell <danplubell@gmail.com>
2024-11-14 08:10:26 -0600

    Update

--- src/model/column.rs
+++ src/model/column.rs
@@ -33,13 +33,15 @@ mod tests {
             "318457962572986143946312578639178425157294836284563791425731689761829354893645217";
         let mut cells = Cells::from(solution);
         let mut column = Column::new();
-
-        for i in 0..9 {
-            column.add_cell(cells.get_at(i).unwrap())
-        }
-
-        for i in 0..9 {
-            assert_eq!(column.get_at(i).value(), cells.get_at(i).unwrap().value())
+        {
+            let chunks = cells.get_chunks(9);
+            let (_, cell_chunk) = chunks.enumerate().next().unwrap();
+            for (i, c) in cell_chunk.iter().enumerate() {
+                column.add_cell(c);
+            }
+            for i in 0..9 {
+                assert_eq!(column.get_at(i).value(), cells.get_at(i).unwrap().value())
+            }
         }
         cells.set_at(0, 9);
         assert_eq!(column.get_at(0).value(), 9)
